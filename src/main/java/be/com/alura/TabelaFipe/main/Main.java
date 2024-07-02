35f0a0477f016ad70a22bbc2b2ef6a02e393e40d
Mateus Perpetuo <mateus_perpetuo@outlook.com>
2024-07-02 11:24:39 -0300

    v.1.0 - Finalização da aplicação

--- src/main/java/be/com/alura/TabelaFipe/main/Main.java
+++ src/main/java/be/com/alura/TabelaFipe/main/Main.java
@@ -3,11 +3,15 @@ package be.com.alura.TabelaFipe.main;
 
 import be.com.alura.TabelaFipe.model.CarModels;
 import be.com.alura.TabelaFipe.model.Data;
+import be.com.alura.TabelaFipe.model.Vehicle;
 import be.com.alura.TabelaFipe.service.ApiConsumer;
 import be.com.alura.TabelaFipe.service.ConvertData;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     Scanner scan = new Scanner(System.in);
@@ -64,8 +68,8 @@ public class Main {
 
         // askin to the api about car models
         System.out.println("\nDigite o código da marca do veículo a ser consultado: ");
-        String carBrandCod = scan.nextLine();
-        adress = adress + "/" + carBrandCod + "/modelos";
+        String vehicleCode = scan.nextLine();
+        adress = adress + "/" + vehicleCode + "/modelos";
 
         // getting the api return and converting the json to a list of car models
         json = apiConsumer.getData(adress);
@@ -73,11 +77,42 @@ public class Main {
 
         System.out.println("\n--- Lista dos CarModels dessa Marca ---\n");
 
+
         carModelsList.modelos().stream()
                 .sorted(Comparator.comparing(data -> Integer.parseInt(data.getCodigo())))
                 .forEach(System.out::println);
 
+        System.out.println("\n Digite o nome do carro a ser consultado: ");
+        var vehicleName = scan.nextLine();
 
+        List<Data> modelsFilter =  carModelsList.modelos().stream()
+                .filter(m -> m.getNome().toLowerCase().contains(vehicleName.toLowerCase()))
+                .sorted(Comparator.comparing(data -> Integer.parseInt(data.getCodigo())))
+                .collect(Collectors.toList());
+
+        System.out.println("\n--- Lista de Modelos filtrados ---\n");
+        modelsFilter.forEach(System.out::println);
+
+        // askin to the api about car models
+        System.out.println("Por favor digite o código do modelo para buscar os valores de avaliaçao: ");
+        vehicleCode = scan.nextLine();
+
+        // getting the api return and converting the json to a list cars years
+        adress =  adress + "/" + vehicleCode + "/anos";
+        json = json = apiConsumer.getData(adress);
+
+        List<Data> years = convertData.getList(json, Data.class);
+        List<Vehicle> vehicleList = new ArrayList<>();
+
+        // Loop to get ans show the car prices
+        for (int i = 0; i< years.size(); i++){
+            var adressYear = adress + "/" + years.get(i).getCodigo();
+            json = apiConsumer.getData(adressYear);
+            Vehicle vehicle = convertData.getData(json, Vehicle.class);
+            vehicleList.add(vehicle);
+        }
+        System.out.println("\n--- Lista dos veículos filtrados com avaliações por ano ---");
+        vehicleList.forEach(System.out::println);
 
     }
 
---
+++ src/main/java/be/com/alura/TabelaFipe/model/Vehicle.java
@@ -0,0 +1,24 @@
+package be.com.alura.TabelaFipe.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Vehicle(
+        @JsonAlias("Modelo") String model,
+        @JsonAlias("Marca") String brand,
+        @JsonAlias("AnoModelo") Integer year,
+        @JsonAlias("Valor") String Price,
+        @JsonAlias("Combustivel") String fuelType
+) {
+
+    @Override
+    public String toString() {
+        return "\nInformações do Veículo"
+                + "\nMODELO - " + model
+                + ", MARCA - " + brand
+                + ", ANO -" + year
+                + ", COMBUSTIVEL - " + fuelType
+                + "\nValor avaliado: R$" + Price;
+    }
+}
